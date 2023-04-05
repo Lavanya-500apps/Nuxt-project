231c8f92955d9f099776ded9885b9eb7fe74a755
Lavanya-500apps <Lavanya.gavara@500apps.com>
2023-04-05 22:03:19 +0530

    CRUD operations for Notes

---
+++ nuxt-project/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+     <h1 class="text-3xl font-bold underline">
+    Hello Nuxt!
+  </h1>
+    <NuxtLayout>
+<NuxtPage/>
+</NuxtLayout>
+<!-- <CollectionMain/> -->
+<!-- <CollectionEdit/>
+<CollectionUpdates/> -->
+  </div>
+</template>
+
